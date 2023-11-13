6fb09247caa4054822107faf2b7c1c319ffb5500
Vish <38266782+humboldt123@users.noreply.github.com>
2023-11-13 17:33:28 -0500

    erm

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # Start Script
 
-# this is the start script! nothing too fancy but we need to use java 8 cos its on 1.18
+# this is the start script! nothing too fancy but we need to use java 8 cos its on 1.16
 # -DIReallyKnowWhatIAmDoingISwear flag to disable annoying warnings (it doesnt work sadly)
 
 java -Xmx1200M -Xms1200M -XX:+UseConcMarkSweepGC -DIReallyKnowWhatIAmDoingISwear -jar paper-1.16.5-794.jar
